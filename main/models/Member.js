ae9c87951535cf2a225934a46d742590f8dec3a6
Craig Wickersham <cw@Craigs-MacBook-Pro.local>
2025-03-09 21:53:14 -0700

    wip

--- main/models/Member.js
+++ main/models/Member.js
@@ -4,6 +4,7 @@ const memberSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   phoneNumber: String,
+  email: String,
   lastGifteeMatch: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }], // Id of last member user was secret santa for
 });
 
